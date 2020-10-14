dc287afada73928b1cbf333fbc34363fc2386db1
AlanBretelle <bretellealan@hotmail.fr>
2020-10-14 23:30:17 +0200

    signal

--- include/string.h
+++ include/string.h
@@ -23,10 +23,10 @@ extern inline char * strcpy(char * dest, const char * src){
 
     /*
         do{
-            a = lodsb(src)
-            si++
+            a = lodsb(S)
+            S++
             stosb(a)
-            di++
+            D++
         }while(a != 0)
     */
 
@@ -50,14 +50,84 @@ extern inline char * strncpy(char * dest, const char *src, int count){
         do{
             if(--c < 0)
                 break
-            a = lodsb(src)
-            si++
+            a = lodsb(S)
+            S++
             stosb(a)
-            di++
+            D++
         }while(a != 0)
     */
 
     return dest;
+}
+
+extern inline char * strcat(char * dest, const char * src){
+    __asm__("cld\n\t"
+        "repne\n\t"
+        "scasb\n\t"
+        "decl %1\n"
+        "1:\tlodsb\n\t"
+        "stosb\n\t"
+        "testb %%al,%%al\n\t"
+        "jne 1b"
+        ::"S" (src), "D" (dest), "a" (0), "c" (0xFFFFFFFF):"si", "di", "ax", "cx"
+    );
+    /*
+        while(c++ > 0){
+            if(lodsb(D) == 0)
+                break;
+            D++
+        }
+        D--
+        do{
+            if(--c < 0)
+                break
+            a = lodsb(S)
+            S++
+            stosb(a)
+            D++
+        }while(a != 0)
+    */
+   return dest;
+}
+
+extern inline char * strncat(char * dest, const char * src, int count){
+    __asm__("cld\n\t"
+        "repne\n\t"
+        "scasb\n\t"
+        "decl %1\n\t"
+        "movl %4,%3\n"
+        "1:\tdecl %3\n\t"
+        "js 2f\n\t"
+        "lodsb\n\t"
+        "stosb\n\t"
+        "testb %%al,%%al\n\t"
+        "jne 1b\n"
+        "2:\txorl %2,%2\n\t" //ajoute un \0 à la fin de la chaine
+        "stosb"
+        ::"S" (src), "D" (dest), "a" (0), "c" (0xFFFFFFFF), "g" (count)
+        : "si", "di", "ax", "cx" 
+    );
+    return dest;
+}
+
+extern inline int strcmp (const char * cs,const char * ct){
+    register int __res __asm__("ax");
+    __asm__("cld\n"
+        "1:\tlodsb\n\t"
+        "scasb\n\t"
+        "jne 2f\n\t"
+        "testb %%al, %%al\n\t"
+        "jne 1b\n\t"
+        "xorl %%eax, %%eax\n\t"
+        "jmp 3f\n"
+        "2:\tmovl $1,%%eax\n\t"
+        "jl 3f\n\t"
+        "negl %%eax\n"
+        "3:"
+        :"=a" (__res):"S" (cs), "D" (ct), 
+        : "si", "di"
+    );
+    return __res;
 }
 
 #endif
